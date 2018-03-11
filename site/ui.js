264dba76ab58192351cf82fb4cbf373138a4c8b5
Гимаев Наиль <gimntut@list.ru>
2018-03-11 11:56:47 +0500

    Готово

--- site/ui.js
+++ site/ui.js
@@ -1,6 +1,29 @@
-(function (){
-	function init(){
-		alert('ok');
-	}
-	webix.ready(init);
+(function() {
+    var uiExt = {
+        func: {
+            step1: function() {
+                $$('pages').setValue('step1');
+            },
+            step2: function() {
+                var original = $$('original').getValue();
+                $$('textForTranslate').setValues({
+                    original: original
+                });
+                $$('pages').setValue('step2');
+            },
+            step3: function() {
+                var html = webix.html.getValue('text-for-translate');
+                $$('result').setValue(html);
+                $$('pages').setValue('step3');
+            }
+        }
+    };
+
+    function init() {
+        var uiAsText = webix.html.getValue('ui');
+        var uiObj = jsyaml.load(uiAsText);
+        var ui = webix.uiPrepare(uiObj, uiExt);
+        webix.ui(ui);
+    }
+    webix.ready(init);
 })()
--- site/webix-ext.js
+++ site/webix-ext.js
@@ -5,7 +5,6 @@ webix.yamlUiPrepare = function yamlUiPrepare(rawUi, uiExt) {
 }
 
 webix.uiPrepare = function(ui, uiExt) {
-    gettext = gettext || function() { return s; };
     for (var key in ui) {
         var item = ui[key];
         if (typeof(item) == "object") {
@@ -22,23 +21,4 @@ webix.uiPrepare = function(ui, uiExt) {
         }
     }
     return ui;
-}
-
-webix.DataDriver.apiJson = webix.copy(webix.DataDriver.json);
-
-webix.DataDriver.apiJson.getRecords = function(data) {
-    if (data && data.results) {
-        data = data.results;
-    }
-    if (data && !webix.isArray(data)) {
-        return [data];
-    }
-    return data;
-}
-
-var _gt = gettext || function() { return s; };
-_strings_for_pseudo_gettext = {}
-
-function _gettext(s) {
-    _strings_for_pseudo_gettext[s] = _gt(s);
 }
