56175f0512e9d58c83e7b0c237371f5559eccc22
Наиль Гимаев <gimntut@list.ru>
2018-11-04 19:59:37 +0500

    --

--- site/ui.js
+++ site/ui.js
@@ -1,26 +1,8 @@
 (function() {
     var uiExt = {
         func: {
-            step1: function() {
-                $$('pages').setValue('step1');
-            },
-            step2: function() {
-                var original = $$('original').getValue();
-                $$('textForTranslate').setValues({
-                    original: original
-                });
-                $$('pages').setValue('step2');
-            },
-            step3: function() {
-                $('#goog-gt-tt, .goog-te-spinner-pos, [style="height: 150px;"]').remove();
-                var item = $('font[style="vertical-align: inherit;"]')[0];
-                while (item) {
-                    item.outerHTML = item.innerHTML;
-                    var item = $('font[style="vertical-align: inherit;"]')[0];
-                }
-                var html = webix.html.getValue('text-for-translate');
-                $$('result').setValue(html);
-                $$('pages').setValue('step3');
+            products: function(){
+                webix.alert('Ok');
             }
         }
     };
